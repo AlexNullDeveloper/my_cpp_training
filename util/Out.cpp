8051f9775ba89cb0a4e16ca38e41c88775bc9031
talismanov <talismanoff1990@yandex.ru>
2017-10-15 18:34:00 +0300

    trying to do println function

---
+++ util/Out.cpp
@@ -0,0 +1,11 @@
+//
+// Created by talismanov on 15.10.2017.
+//
+
+#include "Out.h"
+#include <iostream>
+using namespace std;
+
+void Out::println(string message) {
+    cout << message << endl;
+}
---
+++ util/Out.h
@@ -0,0 +1,17 @@
+//
+// Created by talismanov on 15.10.2017.
+//
+
+#ifndef MY_CPP_TRAINING_OUT_H
+#define MY_CPP_TRAINING_OUT_H
+
+#include <iostream>
+using namespace std;
+
+class Out {
+public:
+    static void println(string message);
+};
+
+
+#endif //MY_CPP_TRAINING_OUT_H
