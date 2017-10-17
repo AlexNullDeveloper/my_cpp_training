a5cc3c22474b502c16414f2419ac91b8906443ff
talismanov <talismanoff1990@yandex.ru>
2017-10-17 09:31:52 +0300

    1. morning commit

--- main.cpp
+++ main.cpp
@@ -1,34 +1,61 @@
+#include <stdlib.h>
 #include <iostream>
-#include <mysql.h>
-MYSQL *connection, mysql;
-MYSQL_RES *result;
-MYSQL_ROW row;
-int query_state;
 
-int main() {
-    mysql_init(&mysql);
-    //connection = mysql_real_connect(&mysql,"host","user",
-    //                   "password","database",port,"unix_socket",clientflag);
-    connection = mysql_real_connect(&mysql,"localhost",
-                                    "root","qwer","cpp_data",3306,0,0);
-    if (connection == NULL) {
-        std::cout << mysql_error(&amp;mysql) << std::endl;
-        return 1;
+/*
+  Include directly the different
+  headers from cppconn/ and mysql_driver.h + mysql_util.h
+  (and mysql_connection.h). This will reduce your build time!
+*/
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+using namespace std;
+
+int main(void)
+{
+    cout << endl;
+    cout << "Running 'SELECT 'Hello World!' AS _message'..." << endl;
+
+    try {
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+
+        /* Create a connection */
+        driver = get_driver_instance();
+        con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+        /* Connect to the MySQL test database */
+        con->setSchema("test");
+
+        stmt = con->createStatement();
+        res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
+        while (res->next()) {
+            cout << "\t... MySQL replies: ";
+            /* Access column data by alias or column name */
+            cout << res->getString("_message") << endl;
+            cout << "\t... MySQL says it again: ";
+            /* Access column data by numeric offset, 1 is the first column */
+            cout << res->getString(1) << endl;
+        }
+        delete res;
+        delete stmt;
+        delete con;
+
+    } catch (sql::SQLException &e) {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line "
+        << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
     }
 
-    query_state = mysql_query(connection, "select user_count()");
-    if (query_state !=0) {
-        std::cout << mysql_error(connection) << std::endl;
-        return 1;
-    }
+    cout << endl;
 
-    result = mysql_store_result(connection);
-    while (( row = mysql_fetch_row(result)) != NULL) {
-        std::cout << "Number of active users : " << row[0] << std::endl;
-    }
-
-    mysql_free_result(result);
-    mysql_close(connection);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
